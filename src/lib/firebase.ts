fe612079b587c665b79d6d36c9607950bec93f9d
Atharva Khond <atharvapkhond@gmail.com>
2025-08-07 00:32:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,14 @@
 // Implemented Firebase configuration and initialization to connect the app with Firebase services.
 import { initializeApp, getApps, getApp } from 'firebase/app';
+import { config } from 'dotenv';
+
+config();
 
 const firebaseConfig = {
   projectId: "pocketai-e4n6o",
   appId: "1:214335210694:web:b00a39827f7234d84243c7",
   storageBucket: "pocketai-e4n6o.firebasestorage.app",
-  apiKey: "AIzaSyC729M_UoMijkTzn0bPQfCFCxkUeefIrc0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyC729M_UoMijkTzn0bPQfCFCxkUeefIrc0",
   authDomain: "pocketai-e4n6o.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "214335210694"
